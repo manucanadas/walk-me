d181b2915964e7610fa10e6399b3feb2fca7075c
student <student@eda-bc-02.localdomain>
2020-11-26 11:50:05 +1300

    migrations made

---
+++ server/db/migrations/20201126094513_walks_tags.js
@@ -0,0 +1,11 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('walks_tags', table => {
+    table.increments('id')
+    table.integer('walk_id')
+    table.integer('tag_id')
+  })
+}
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('walks_tags')
+}
