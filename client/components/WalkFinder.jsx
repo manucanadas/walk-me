9a8b9f7cb55a3b083a88401ad172ccdfd660977d
htense <hortenserothery@gmail.com>
2020-11-27 12:11:44 +1300

    ready to merge nav links with new components

---
+++ client/components/WalkFinder.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import BigMap from './BigMap'
+import WalkList from './WalkList'
+
+const WalkFinder = () => {
+  return (
+
+    <>
+      <h1>THE WALK LIST & Map component WORKS</h1>
+      <h4>After you search, this shows the Map and list of the walks </h4>
+      <BigMap/>
+      <WalkList/>
+    </>
+
+  )
+}
+
+export default WalkFinder
