feaf1b4380b069fd558fbb3df3fa061e354432ed
htense <hortenserothery@gmail.com>
2020-11-26 16:26:31 +1300

    change component search to home

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 
 
-const Search = () => {
+const Home = () => {
   return (
 <>
 <h1>THE SEARCH COMP WORKS</h1>
@@ -16,4 +16,4 @@ const Search = () => {
   )
 }
 
-export default Search
+export default Home
