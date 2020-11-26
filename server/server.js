77d704c96e43f65b65180b7f13e86bf7f2efd2ee
student <student@eda-bc-03.localdomain>
2020-11-26 16:02:30 +1300

    applyauthroutes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const fruitRoutes = require('./routes/fruits')
+const authRoutes = require('./routes/auth')
 
 const server = express()
 
@@ -9,5 +10,7 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/fruits', fruitRoutes)
+server.use('/api/v1', authRoutes)
+
 
 module.exports = server
