a2e8930c149daa5cce0d3ae34b327473ccc95e7c
student <student@pop-os.localdomain>
2020-11-26 15:04:03 +1300

    completed seed data

--- server/db/seeds/walk_tags.js
+++ server/db/seeds/walk_tags.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('walk_tags').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('walk_tags').insert([
+        {id: 1, walk_id: 2, tag_id: 1},
+        {id: 2, walk_id: 3, tag_id: 1},
+        {id: 3, walk_id: 1, tag_id: 1}
       ]);
     });
 };
