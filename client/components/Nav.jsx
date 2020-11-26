864d99d8eccc7bac880de32db5a64538472a5f5e
student <student@pop-os.localdomain>
2020-11-27 11:21:00 +1300

    update nav links in nav.jsx

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 const Nav = () => {
   return (
     <>
       <h4>Navigation</h4>
       <nav className="main-nav" >
         <ul className="main-nav__items" >
-          <li className="main-nav__item"><Link to='/' className="nav-link"> Home</Link></li>
-          <li className="main-nav__item"><Link to='/burger' className="nav-link"> Burgers</Link></li>
-          <li className="main-nav__item main-nav__item--cta"><Link to='/burger/new' className="nav-link"> Add Burger</Link></li>
+          
+          <li className="main-nav__item"><NavLink to='/' exact activeClassName="active" className="nav-link"> Home</NavLink></li>
+          {/* <li className="main-nav__item"><NavLink to='/walks' exact activeClassName="active" className="nav-link"> WalkFinder</NavLink></li> --not sure whether this should be a navlink, or re-direct instead as this walks page will appear based on what the user searches on the homepage*/}
+          <li className="main-nav__item"><NavLink to='/walks/all' activeClassName="active" className="nav-link"> All Walks</NavLink></li>
+
+          {/* <li className="main-nav__item"><NavLink to='/walks/saved' activeClassName="active" className="nav-link"> Saved Walks</NavLink></li> -- this should only be visible to authenticated users so not always showing on nav bar */}
+          
+          <li className="main-nav__item"><NavLink to='/login' activeClassName="active" className="nav-link"> Login</NavLink></li>
+          <li className="main-nav__item"><NavLink to='/register' activeClassName="active" className="nav-link"> Register</NavLink></li>
+
+          {/* <li className="main-nav__item"><NavLink to='/' activeClassName="active" className="nav-link"> Sign out</NavLink></li> - this also is only visible when users are authenticated*/}
+
+
+ 
         </ul>
       </nav>
     </>
