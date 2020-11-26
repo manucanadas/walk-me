9234c689dd329fd4b842e5b017b5b5bbc5cf0f97
matt-blackbourn <matt.blackie114@gmail.com>
2020-11-26 20:41:37 +1300

    fruits removed

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const path = require('path')
 
-const fruitRoutes = require('./routes/fruits')
 const walkRoutes = require('./routes/walkRoutes')
 const userRoutes = require('./routes/userRoutes')
 
@@ -10,7 +9,6 @@ const server = express()
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-server.use('/api/v1/fruits', fruitRoutes)
 server.use('/api/v1/walks', walkRoutes)
 server.use('/api/v1/user', userRoutes)
 
