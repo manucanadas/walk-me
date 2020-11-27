8f3d4803f49558ec4e036f5731f47722d8dab10a
htense <hortenserothery@gmail.com>
2020-11-27 14:11:18 +1300

    fixer upper

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -4,10 +4,10 @@ const Nav = () => {
   return (
     <>
       <h4>Navigation</h4>
-      <nav className="main-nav" >
+      <nav className='nav-box'>
         <ul className="main-nav__items" >
 
-          <li className="main-nav__item"><NavLink to='/' exact activeClassName="active" className="nav-link"> Home</NavLink></li>
+          <span className="main-nav__item"><NavLink to='/' exact activeClassName="active" className="nav-link"> Home</NavLink></span>
           {/* <li className="main-nav__item"><NavLink to='/walks' exact activeClassName="active" className="nav-link"> WalkFinder</NavLink></li> --not sure whether this should be a navlink, or re-direct instead as this walks page will appear based on what the user searches on the homepage */}
           <li className="main-nav__item"><NavLink to='/walks/all' activeClassName="active" className="nav-link"> All Walks</NavLink></li>
 
