01a32e58a48317bb5298fca5529446154f2ceed0
emily-berryman <42923006+emily-berryman@users.noreply.github.com>
2020-11-27 14:33:18 +1300

    add kelly's review changes of development branch into master

--- client/reducers/auth.test.js
+++ client/reducers/auth.test.js
@@ -1,10 +1,24 @@
 import auth from './auth'
 
 describe('auth reducer tests', () => {
-  test('empty (initial) state', () => {
+  test('default case', () => {
     const expected = []
 
     const actual = auth([], {})
+
+    expect(actual).toEqual(expected)
+  })
+
+  test('initial state', () => {
+    const expected = {
+      loggedIn: true,
+      user: {
+        id: 1,
+        username: 'walkymcwalkface'
+      }
+    }
+
+    const actual = auth(undefined, {})
 
     expect(actual).toEqual(expected)
   })
