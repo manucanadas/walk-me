f1c8aac5ecd09d2a6881b329ea9b02b7a496dafb
student <student@eda-bc-02.localdomain>
2020-11-27 15:30:37 +1300

    commenting out irrelevant tests

--- client/apis/walks.test.js
+++ client/apis/walks.test.js
@@ -26,20 +26,20 @@ jest.mock('../../server/db/walksDb', () => ({
 
   getSavedWalksByUser: (id) => Promise.resolve(
     { id: 1, user_id: 1, walk_id: 2, completed: true }
-  )
+  ),
 
-  // getWalkComments: (id) => Promise.resolve(
-  //   {
-  //     id: 1,
-  //     date: new Date().getTime(),
-  //     user_id: 2,
-  //     text: 'terrible walk, was fuck*n raining the entire time',
-  //     img:'image.png',
-  //     enjoyment: 1,
-  //     walk_id: 1,
-  //     flagged: true
-  //   }
-  // )
+  getWalkComments: (id) => Promise.resolve(
+    {
+      id: 1,
+      date: new Date().getTime(),
+      user_id: 2,
+      text: 'terrible walk, was fuck*n raining the entire time',
+      img: 'image.png',
+      enjoyment: 1,
+      walk_id: 1,
+      flagged: true
+    }
+  )
 
 }))
 
@@ -51,39 +51,42 @@ test('/api/v1/walks returns all walks', () => {
     .expect('Content-Type', /json/)
     .expect(200)
     .then(res => {
-      return expect(res.body).toHaveLength(expected)
+      expect(res.body).toHaveLength(expected)
+      return null
     })
     .catch(err => {
       expect(err).toBeFalsy()
     })
 })
 
-// tests route returns a walk by id
+// // tests route returns a walk by id
 // test('/api/v1/walks/:id returns a walk by id', () => {
-//   const expected = 'Northern Walkway'
+//   // const expected = 'Northern Walkway'
 //   return request(server)
 //     .get('/api/v1/walks/2')
 //     .expect('Content-Type', /json/)
 //     .expect(200)
 //     .then(res => {
 //       expect(res.body.id).toBe(2)
-//       expect(res.body.title).toBe(expected)
+//       // expect(res.body.title).toBe(expected)
+//       return null
 //     })
 //     .catch(err => {
 //       expect(err).toBeFalsy()
 //     })
 // })
 
-// tests route returns all saved walks by user
+// // tests route returns all saved walks by user
 // test('/api/v1/walks/saved/:id', () => {
-//   const expected = 2
+//   // const expected = 2
 //   return request(server)
 //     .get('/api/v1/saved/1')
 //     .expect('Content-Type', /json/)
 //     .expect(200)
 //     .then(res => {
 //       expect(res.body.user_id).toBe(1)
-//       expect(res.body.walk_id).toBe(expected)
+//       // expect(res.body.walk_id).toBe(expected)
+//       return null
 //     })
 //     .catch(err => {
 //       expect(err).toBeFalsy()
