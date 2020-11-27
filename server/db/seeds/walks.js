8a7f72b4406e01a3f709000e808f4cba0cb92b12
brenmurrell <bren@moon.co.nz>
2020-11-27 14:59:17 +1300

    switch lat/long around for mapbox format

--- server/db/seeds/walks.js
+++ server/db/seeds/walks.js
@@ -18,7 +18,7 @@ exports.seed = function (knex) {
             '[[-122.48369693756104, 37.83381888486939],[-122.48348236083984, 37.83317489144141],[-122.48339653015138, 37.83270036637107],[-122.48356819152832, 37.832056363179625]]',
           img:
             'https://www.wellingtonregionaltrails.com/assets/Trails/Elements/Images/Makara-walkway-view__FocusFillWzcyMCw0NjgsInkiLDVd.jpg',
-          coords: '-41.232617, 174.692406'
+          coords: '[174.692406, -41.232617 ]'
         },
         {
           id: 2,
@@ -33,7 +33,7 @@ exports.seed = function (knex) {
             '[[-122.48369693756104, 37.83381888486939],[-122.48348236083984, 37.83317489144141],[-122.48339653015138, 37.83270036637107],[-122.48356819152832, 37.832056363179625]]',
           img:
             'https://cdn-assets.alltrails.com/uploads/photo/image/26349935/extra_large_8028e442d50e05abf799fbea94115730.jpg',
-          coords: '-41.247232, 174.773512'
+          coords: ['174.773512, -41.247232']
         },
         {
           id: 3,
@@ -47,7 +47,7 @@ exports.seed = function (knex) {
             '[[-122.48369693756104, 37.83381888486939],[-122.48348236083984, 37.83317489144141],[-122.48339653015138, 37.83270036637107],[-122.48356819152832, 37.832056363179625]]',
           img:
             'https://www.wellingtonregionaltrails.com/assets/Trails/Elements/Images/east-harbour-regional-park-kaeaea-track-summit__FocusFillWzcyMCw0NjgsInkiLDVd.jpg',
-          coords: '-41.297824, 174.904689'
+          coords: '[174.904689, -41.297824]'
         }
       ])
     })
