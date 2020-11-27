c31829104da7144369a277c94654d213a7c47067
htense <hortenserothery@gmail.com>
2020-11-27 16:46:39 +1300

    update nav and home

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const Home = () => {
   return (
     <>
+      <div className= 'walk-image-and-title2'>
+<img src='https://i.pinimg.com/originals/da/5a/19/da5a19b584d9acc95f9984dc5061239c.jpg' alt="description"/>
+
+<div className= 'walk-title-div'>
       <div className="search-box-div">
         <form className="search-box" action="./walks">
           <input className='search' type="text" placeholder="Search.." name="search" />
           <button type="submit"><i className="fa fa-search"></i></button>
         </form>
       </div>
-      <div className= 'walk-image-and-title2'>
-<img src='https://i.pinimg.com/originals/da/5a/19/da5a19b584d9acc95f9984dc5061239c.jpg' alt="description"/>
-
-<div className= 'walk-title-div'>
   <span className= 'walk-title' >Lets go walking!!</span>
 </div>
 </div>
